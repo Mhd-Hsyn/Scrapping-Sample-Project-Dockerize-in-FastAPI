8ec337eecedf489ddf657791962b0464233657b8
Your Name <you@example.com>
2024-03-15 00:24:09 +0500

    allow cross origin in project

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 from script.script import start_scrapping
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 @app.get("/")
 def read_root():
